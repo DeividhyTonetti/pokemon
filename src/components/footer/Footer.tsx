95b33ce96b7e4a6103be8176914165dfd0f2fc42
Deividhy Tonetti <deividhytonetti@gmail.com>
2022-06-18 18:37:15 -0300

    [Feat]: Inclusão de componentes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,8 @@
 import React, { FC } from "react";
 
 import {
-    Badge,
     CardContainer,
-    CardUser,
-    Title,
     ImageContainer,
-    ImageCard,
 } from './styles';
 
 export type TopCardProps = {
@@ -19,7 +15,7 @@ export type TopCardProps = {
     onClick?: () => void
 }
 
-const TopCard: FC = (props: TopCardProps) => {
+const Footer: FC = (props: TopCardProps) => {
 
     return (
         <>
@@ -28,20 +24,10 @@ const TopCard: FC = (props: TopCardProps) => {
                 curve={props.curve}
                 cardSize={props.cardSize}
             >
-                <Title>
-                    <h1>
-                        Olá, Deividhy Tonetti
-                        <span>
-                            Bem Vindo! &#128515;
-                        </span>
-                    </h1>
-                </Title>
-                <ImageContainer>
-                    
-                </ImageContainer>
+                <ImageContainer />
             </CardContainer>
         </>
     )
 }
 
-export { TopCard }
+export { Footer }
